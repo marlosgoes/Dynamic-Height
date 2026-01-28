a35dec12534b7898f4ed7a185f5745a13891ebb4
marlosgoes <marlos.goes@noaa.gov>
2026-01-28 11:46:44 -0500

    Modify add_abs_gpan to handle 3-D fields
    Updated function to accept 3-D fields and clarified input/output descriptions. Adjusted calculations for absolute geopotential height.

--- add_abs_gpan.m
+++ add_abs_gpan.m
@@ -1,8 +1,24 @@
 function [gpan,addep2,addep] = add_abs_gpan(gpan,lat2,lon2,Pn,pref,addep,month_in)
 %NOW ACCEPTS 3-D filed
-%DEPTH HAS TO BE FIRST DIMENSION
+%DEPTH HAS TO BE FIRST DIMENSION IN GPAN
+
+% INPUT: gpan- geopotential height (in cm)
+%        lat2- latitude array
+%        lon2- longitude array
+%        Pn- Pressure (db)
+%        pref- refernece pressure (db) [optional]
+%        month_in- month (1-12) [optional]
+% OUTPUT:
+%        gpan-Absolute geopotential height
+%        addep- Absolute geopotential height at reference level
+%        addep2- Absolute geopotential height at reference level (same size as INPUT)
+
+%By Marlos Goes
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 sizeg = size(gpan);
-%dep = find(Pn==pref);
+
+%Match depth reference;
 [~,dep] = min(abs(Pn-pref));
 
 ndep = length(Pn);
@@ -19,7 +35,7 @@ if exist('month_in','var')
     
     
 elseif exist('addep','var')
-
+   disp('Don't load')
 else    
     
     addep = load_abs_gpan_section(lat2,lon2,Pn);
@@ -31,14 +47,10 @@ else
     
 end
 
-%gpan = gpan - ones(ndep,1)*gpan(dep,:);
-% 'add' %,keyboard
-gpan = ones(ndep,1)*gpan(1,:)-gpan(:,:);             % display('Mudei ADDEP')
-gpan = ones(ndep,1)*gpan(dep,:)-gpan(:,:);       %     display('Mudei ADDEP')
+% 'add absolute DH to the reference level' 
+gpan = ones(ndep,1)*gpan(1,:)-gpan(:,:);             % Reference to zero
+gpan = ones(ndep,1)*gpan(dep,:)-gpan(:,:);           % Reference to absolute
 gpan = gpan + ones(ndep,1)*addep(dep,:);
-%keyboard
-%gpan = gpan - ones(ndep,1)*addep(dep,:);
-
 gpan = reshape(gpan,sizeg);
 addep2 = reshape(addep,sizeg);
 
